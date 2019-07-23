b8e1e70548ce4443ea4f69fee2bf011a2a3c14fb
Misaki Koonce <misaki.koonce@gmail.com>
2019-07-23 16:38:07 -0700

    Feat: Add functions for state.

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -3,6 +3,39 @@ import { View } from 'react-native';
 import { getMetricMetaInfo } from '../utils/helpers';
 
 class AddEntry extends Component {
+    state = {
+        run: 0,
+        bike: 0,
+        swim: 0,
+        sleep: 0,
+        eat: 0,
+    }
+    increment = () => {
+        const { max, step } = getMetricMetaInfo(metric)
+        this.setState(state => {
+            const count = state[metric] + step
+            return {
+                ...state,
+                [metric]: count > max ? max : count,
+            }
+        })
+    }
+
+    decrement = () => {
+        this.setState(state => {
+            const count = state[metric] - getMetricMetaInfo(metric).step
+            return {
+                 ...state,
+                [metric]: count < 0 ? 0 : count,
+            }
+        })
+    }
+    slide = (metric, value) => {
+        this.setState(state => ({
+            [metric]: value,
+        }))
+    }
+
     render() {
         return (
             <View>
