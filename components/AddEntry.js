d6de5b857ed225eb4d6a9b0d13bc7947a218f431
Misaki Koonce <misaki.koonce@gmail.com>
2019-07-23 17:15:08 -0700

    Feat: Render UdaciSlider or UdaciSteppers based on condition.

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { getMetricMetaInfo } from '../utils/helpers';
+import UdaciSlider from './UdaciSlider';
+import UdaciSteppers from './UdaciSteppers'
 
-class AddEntry extends Component {
+export default class AddEntry extends Component {
     state = {
         run: 0,
         bike: 0,
@@ -37,12 +39,33 @@ class AddEntry extends Component {
     }
 
     render() {
+        const metaInfo = getMetricMetaInfo();
+
         return (
             <View>
-                {getMetricMetaInfo('bike').getIcon()}
+                {Object.keys(metaInfo).map(key => {
+                    const { getIcon, type, ...rest } = metaInfo[key]
+                    const value = this.state[key]
+                    return(
+                        <View key={key}>
+                            {getIcon()}
+                            {type === 'slider'
+                             ? <UdaciSlider
+                                    value={value}
+                                    onChange={value => this.slide(key, value)}
+                                    {...rest}
+                                />
+                             : <UdaciSteppers
+                                    value={value}
+                                    onIncrement={() => this.increment(key)}
+                                    onDecrement={() => this.decrement(key)}
+                                    {...rest}
+                                />
+                            }
+                        </View>
+                    )
+                })}
             </View>
         )
     }
 }
-
-export default AddEntry;
