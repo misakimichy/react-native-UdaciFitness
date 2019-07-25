248345fda9199b9caf4883c262f7cf937ffc7177
Misaki Koonce <misaki.koonce@gmail.com>
2019-07-25 10:53:59 -0700

    Feat: Add submit function.

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { getMetricMetaInfo } from '../utils/helpers';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { getMetricMetaInfo, timeToString } from '../utils/helpers';
 import UdaciSlider from './UdaciSlider';
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader';
+
+function SubmitBtn ({ onPress }) {
+    return (
+        <TouchableOpacity
+            onPress={onPress}
+        >
+            <Text>Submit</Text>
+        </TouchableOpacity>
+    )
+}
 
 export default class AddEntry extends Component {
     state = {
@@ -34,9 +44,28 @@ export default class AddEntry extends Component {
         })
     }
     slide = (metric, value) => {
-        this.setState(state => ({
+        this.setState(() => ({
             [metric]: value,
         }))
+    }
+
+    submit = () => {
+        const key = timeToString();
+        const entry = this.state;
+        
+        // Update Redux
+        this.setState(() => ({
+            run: 0,
+            bike: 0,
+            swim: 0,
+            sleep: 0,
+            eat: 0,
+        }))
+
+        // Navigate to Home
+
+        //Save to 'DB'
+        // Clear local notification
     }
 
     render() {
@@ -69,6 +98,7 @@ export default class AddEntry extends Component {
                         </View>
                     )
                 })}
+                <SubmitBtn onPress={this.submit}/>
             </View>
         )
     }
