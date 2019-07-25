de459ec5b7664d125ea092199e68ed6756310293
Misaki Koonce <misaki.koonce@gmail.com>
2019-07-25 14:29:30 -0700

    Feat: Import functions from api.js

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { getMetricMetaInfo, timeToString } from '../utils/helpers';
 import UdaciSlider from './UdaciSlider';
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader';
+import TextButton from './TextButton';
+import { submitEntry, removeEntry } from '../utils/api';
 
 function SubmitBtn ({ onPress }) {
     return (
-        <TouchableOpacity
-            onPress={onPress}
-        >
+        <TouchableOpacity onPress={onPress}>
             <Text>Submit</Text>
         </TouchableOpacity>
-    )
+    );
 }
 
 export default class AddEntry extends Component {
@@ -22,17 +23,18 @@ export default class AddEntry extends Component {
         swim: 0,
         sleep: 0,
         eat: 0,
-    }
-    increment = metric => {
+    };
+
+    increment = metric => {submitEntry({ key, entry })
         const { max, step } = getMetricMetaInfo(metric)
         this.setState(state => {
             const count = state[metric] + step
             return {
                 ...state,
                 [metric]: count > max ? max : count,
-            }
-        })
-    }
+            };
+        });
+    };
 
     decrement = metric => {
         this.setState(state => {
@@ -40,14 +42,15 @@ export default class AddEntry extends Component {
             return {
                  ...state,
                 [metric]: count < 0 ? 0 : count,
-            }
-        })
-    }
+            };
+        });
+    };
+
     slide = (metric, value) => {
         this.setState(() => ({
             [metric]: value,
-        }))
-    }
+        }));
+    };
 
     submit = () => {
         const key = timeToString();
@@ -65,20 +68,39 @@ export default class AddEntry extends Component {
         // Navigate to Home
 
         //Save to 'DB'
+        submitEntry({ key, entry })
+
         // Clear local notification
-    }
+    };
+
+    reset = () => {
+        const key = timeToString();
+        
+        // Update Redux
+        // Route to Home
+        // Update 'DB'
+        removeEntry(key);
+    };
 
     render() {
         const metaInfo = getMetricMetaInfo();
 
+        if(this.props.alreadyLogged) {
+            return (
+                <View>
+                    <Ionicons name='ios-happy-outline' size={100} />
+                    <Text>You already logged your info for today!</Text>
+                    <TextButton onPress={this.reset}>Reset</TextButton>
+                </View>
+            );
+        }
+
         return (
             <View>
-                <DateHeader
-                    date={(new Date()).toLocaleDateString()}
-                />
+                <DateHeader date={new Date().toLocaleDateString()} />
                 {Object.keys(metaInfo).map(key => {
-                    const { getIcon, type, ...rest } = metaInfo[key]
-                    const value = this.state[key]
+                    const { getIcon, type, ...rest } = metaInfo[key];
+                    const value = this.state[key];
                     return(
                         <View key={key}>
                             {getIcon()}
@@ -96,7 +118,7 @@ export default class AddEntry extends Component {
                                 />
                             }
                         </View>
-                    )
+                    );
                 })}
                 <SubmitBtn onPress={this.submit}/>
             </View>
