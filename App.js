430e78bc33536fdb4e15612e3cc477263658afc9
Misaki Koonce <misaki.koonce@gmail.com>
2019-07-28 14:04:16 -0700

    Feat: Add navigation tab using react-navigation.

--- App.js
+++ App.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { createAppContainer, createBottomTabNavigator, createMaterialTopTabNavigator } from 'react-navigation';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import AddEntry from './components/AddEntry';
 import reducer from './reducers';
-import History from './components/History'
+import History from './components/History';
+import { purple, white } from './utils/colors';
+
+const Tabs = {
+  History: {
+    screen: History,
+    navigationOption: {
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tinColor }) => <Ionicons name='ios-bookmarks' size={30} color={tinColor} />
+    },
+  },
+  AddEntry: {
+    screen: AddEntry,
+    navigationOption: {
+      tabBarLabel: 'Add Entry',
+      tabBarIcon: ({ tinColor }) => <FontAwesome name='plus-square' size={38} color={tinColor} />
+    },
+  },
+};
+
+const NavigationOptions = {
+  tabBarOptions: {
+    showIcon: true,
+    activeTinColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+          width: 0,
+          height: 3
+      },
+    shadowRadius: 6,
+    shadowOpacity: 1,
+    },
+  },
+};
+
+const TabNav = createAppContainer(Platform.OS === 'ios'
+  ? createBottomTabNavigator(Tabs, NavigationOptions)
+  : createMaterialTopTabNavigator (Tabs, NavigationOptions))
 
 export default class App extends Component {
   render() {
@@ -12,7 +54,7 @@ export default class App extends Component {
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
           <View style={{height: 20}}/>
-          <History />
+          <TabNav />
         </View>
       </Provider>
     );
