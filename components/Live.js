aab2de42e4dff5cced5875f1ddf6bbd0f7090fc9
Misaki Koonce <misaki.koonce@gmail.com>
2019-07-31 22:30:53 -0700

    Feat: Add 'Undetermined' content.

--- components/Live.js
+++ components/Live.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native';
+import { Foundation } from '@expo/vector-icons';
+import { purple, white } from '../utils/colors';
 
 export default class Live extends Component {
     state = {
         coords: null,
-        status: null,
+        status: 'undetermined',
         direction: '',
     }
+    askPermission = () => {
+
+    }
+
     render() {
         const { status, coords, direction } = this.state;
         if (status === null) {
@@ -23,8 +29,12 @@ export default class Live extends Component {
 
         if (status === 'undetermined'){
             return (
-                <View>
-                    <Text>Undetermined</Text>
+                <View style={styles.center}>
+                    <Foundation name='alert' size={50} />
+                    <Text>You need to enable location services for this app.</Text>
+                    <TouchableOpacity onPress={this.askPermission} style={styles.button}>
+                        <Text style={styles.buttonText}>Enable</Text>
+                    </TouchableOpacity>
                 </View>
             )
         }
@@ -37,3 +47,28 @@ export default class Live extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-between',
+    },
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 30,
+        marginRight: 30,
+    },
+    button: {
+        padding: 10,
+        backgroundColor: purple,
+        alignSelf: 'center',
+        borderRadius: 5,
+        margin: 20,
+    },
+    buttonText: {
+        color: white,
+        fontSize: 20,
+    }
+})
